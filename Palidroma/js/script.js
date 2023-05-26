888c7ae89a307f8384eac92b8033ede007e3a063
robertobenfini <roberto.benfini@gmail.com>
2023-05-26 10:09:26 +0200

    funzione-es-palindroma

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -2,23 +2,19 @@
 let utente = (prompt('Inserisci una parola').toLowerCase());
 console.log(utente)
 
-//array
-let array = utente.split('');
-console.log(array)
+//richiamo funzione
+checkPalindroma(utente);
 
-//array reverse
-let array_reverse = array.reverse('');
-console.log(array_reverse)
-
-//parola utente al contrario
-let array_reverse_join = array_reverse.join('');
-console.log(array_reverse_join)
-
-//verifica palindroma
-if(array_reverse_join === utente){
-    console.log('Parola Palindroma')
-}
-
-else{
-    console.log('Parola non Palindroma')
-}
+//funzione palindroma
+function checkPalindroma(parola){
+    
+    let palindroma = parola.split('').reverse().join('');
+    
+    if(palindroma === parola){
+        console.log('Parola Palindroma')
+    }
+    
+    else{
+        console.log('Parola non Palindroma')
+    }
+};
